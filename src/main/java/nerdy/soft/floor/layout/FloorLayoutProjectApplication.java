6f7a48f4305651deaf2cc83d67fe0ad669d73b81
nastya <cctvporsh@gmail.com>
2020-07-13 12:44:04 +0300

    added config files

---
+++ src/main/java/nerdy/soft/floor/layout/FloorLayoutProjectApplication.java
@@ -0,0 +1,13 @@
+package nerdy.soft.floor.layout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorLayoutProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloorLayoutProjectApplication.class, args);
+    }
+
+}
